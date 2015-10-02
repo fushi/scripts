a3a133761ebdb9a7658e8b294c803fe468348b23
John Towler <jtowler@apple.com>
2015-10-01 22:10:59 -0700

    Added script to install all public keys from a given github account.

---
+++ install_keys.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Install this in a crontab
+# { crontab -l | grep -v `install_keys.sh`; echo "*/5 * * * * $FULLPATH"; } | crontab -
+
+usage () {
+    echo "usage: `basename $0` github_username"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || usage
+
+AUTH_KEYS_FILE=~/.ssh/authorized_keys
+
+URL=https://github.com/$1.keys
+
+TEMP_FILE=`mktemp`
+
+echo "Gathering keys from $URL"
+
+curl -sf $URL > $TEMP_FILE
+
+CURL_RET_VAL=$?
+
+if [ "$CURL_RET_VAL" -eq 0 ]
+    then
+        echo "Moving keys into $AUTH_KEYS_FILE"
+        mv $TEMP_FILE $AUTH_KEYS_FILE
+    else
+        echo "Something went wrong with curl. Exiting"
+        rm $TEMP_FILE
+fi
+
+exit $CURL_RET_VAL
